8a29a079d2164596a1f8cbc25e0c3263ae585c07
Mohosin <mohosin16103308@gmail.com>
2019-07-17 16:29:35 +0600

    Update Serilize

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,7 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
-// use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\Groups;
 /**
  * @ApiResource(
  *     collectionOperations={},
@@ -38,7 +38,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"read"})
+ 
      */
     private $password;
 
