da6576dd7b5b0b0df4a8531123be66719a31838e
Mohosin <mohosin16103308@gmail.com>
2019-07-19 23:27:10 +0600

    Added JWT Authenticaion

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
